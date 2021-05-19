8760cc87c38fea8b87a9d18edba48be35cb058fb
elisabeth-ms <emenende@pa.uc3m.es>
2021-05-19 19:58:46 +0200

    Adding ik solver to MapHumanHandTrajectoryToRobotEndEffector in process.

--- programs/MapHumanHandTrajectoryToRobotEndEffector/MapHumanHandTrajectoryToRobotEndEffector.cpp
+++ programs/MapHumanHandTrajectoryToRobotEndEffector/MapHumanHandTrajectoryToRobotEndEffector.cpp
@@ -5,6 +5,54 @@
 #include <sstream>
 #include <nlopt.hpp>
 #include <math.h>
+
+#include <kdl/chain.hpp>
+#include <kdl/jntarray.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/chainiksolverpos_nr_jl.hpp>
+
+/*** TODO: Include this functions in a common library ***/
+static KDL::Chain makeTeoTrunkAndRightArmKinematicsFromDH()
+{
+    const KDL::Joint rotZ(KDL::Joint::RotZ);
+    KDL::Chain chain;
+    chain.addSegment(KDL::Segment(rotZ, KDL::Frame::DH(0.0, -KDL::PI / 2, 0.1932, 0.0)));
+    chain.addSegment(KDL::Segment(rotZ, KDL::Frame::DH(0.305, 0.0, -0.34692, -KDL::PI / 2)));
+    chain.addSegment(KDL::Segment(rotZ, KDL::Frame::DH(0, -KDL::PI / 2, 0, 0)));
+    chain.addSegment(KDL::Segment(rotZ, KDL::Frame::DH(0, -KDL::PI / 2, 0, -KDL::PI / 2)));
+    chain.addSegment(KDL::Segment(rotZ, KDL::Frame::DH(0, -KDL::PI / 2, -0.32901, -KDL::PI / 2)));
+    chain.addSegment(KDL::Segment(rotZ, KDL::Frame::DH(0, KDL::PI / 2, 0, 0)));
+    chain.addSegment(KDL::Segment(rotZ, KDL::Frame::DH(0, -KDL::PI / 2, -0.215, 0)));
+    chain.addSegment(KDL::Segment(rotZ, KDL::Frame::DH(-0.09, 0, 0, -KDL::PI / 2)));
+
+    return chain;
+}
+
+void makeQLimitsTeoTrunkAndRightArmKinematics(KDL::JntArray & qmin, KDL::JntArray & qmax)
+{
+    qmin.resize(8);
+    qmax.resize(8);
+    qmin(0) = -59.3*KDL::deg2rad;
+    qmin(1) = -15.4*KDL::deg2rad;
+    qmin(2) = -98.1*KDL::deg2rad;
+    qmin(3) = -75.5*KDL::deg2rad;
+    qmin(4) = -80.1*KDL::deg2rad;
+    qmin(5) = -99.6*KDL::deg2rad;
+    qmin(6) = -80.4*KDL::deg2rad;
+    qmin(7) = -115.1*KDL::deg2rad;
+    qmax(0) = 46.3*KDL::deg2rad;
+    qmax(1) = 10.1*KDL::deg2rad;
+    qmax(2) = 106.0*KDL::deg2rad;
+    qmax(3) = 22.4*KDL::deg2rad;
+    qmax(4) = 57.0*KDL::deg2rad;
+    qmax(5) = 98.4*KDL::deg2rad;
+    qmax(6) = 99.6*KDL::deg2rad;
+    qmax(7) = 44.7*KDL::deg2rad;
+
+}
+/*** ------------------------------------------------------------------------------------------------ ***/
+
 namespace sharon
 {
     std::vector<std::array<double, 8>> getTrajectoryFromCsvFile(const std::string &filename)
@@ -49,9 +97,9 @@ namespace sharon
         }
     }
 
-    std::vector<double> getTrajectoryPoses(const std::vector<std::array<double, 8>> &trajectoryData, const unsigned int &numPoses)
+    void getTrajectoryPoses(const std::vector<std::array<double, 8>> &trajectoryData, const unsigned int &numPoses, double *discreteTrajectory, unsigned int &size)
     {
-        std::vector<double> desiredTrajectory;
+        //std::vector<double> desiredTrajectory;
 
         unsigned int step = trajectoryData.size() / numPoses;
         std::cout << "size: " << trajectoryData.size() << std::endl;
@@ -64,7 +112,8 @@ namespace sharon
         {
             for (unsigned int i = 1; i < trajectoryData[iPose].size(); i++)
             {
-                desiredTrajectory.push_back(trajectoryData[iPose][i]);
+                discreteTrajectory[(i - 1) + 7 * count] = trajectoryData[iPose][i];
+                //desiredTrajectory.push_back(trajectoryData[iPose][i]);
             }
             count++;
             iPose += step;
@@ -72,94 +121,138 @@ namespace sharon
 
         for (unsigned int i = 1; i < trajectoryData[0].size(); i++)
         {
-            desiredTrajectory.push_back(trajectoryData.back()[i]);
+            discreteTrajectory[(i - 1) + 7 * count] = trajectoryData.back()[i];
         }
-
-        return desiredTrajectory;
     }
 
-    double optimizationFunction(const std::vector<double> &x, std::vector<double> &grad, void *data)
+    double optimizationFunction(unsigned n, const double *x, double *grad, void *data)
     {
         auto *voidToVector = reinterpret_cast<std::vector<double> *>(data);
-        //std::cout << (*voidToVector)[0] << " " << (*voidToVector)[1] << " " << (*voidToVector)[2] << " " << (*voidToVector)[3] << " " << (*voidToVector)[4] << " " << (*voidToVector)[5] << " " << (*voidToVector)[6] << std::endl;
+        std::cout << (*voidToVector)[0] << " " << (*voidToVector)[1] << " " << (*voidToVector)[2] << " " << (*voidToVector)[3] << " " << (*voidToVector)[4] << " " << (*voidToVector)[5] << " " << (*voidToVector)[6] << std::endl;
+        std::cout << x[0] << " " << x[1] << " " << x[2] << " " << x[3] << " " << x[4] << " " << x[5] << " " << x[6] << std::endl;
         double sum = 0;
-        for (unsigned int i = 0; i < x.size(); i++)
+        for (unsigned int i = 0; i < n; i++)
         {
             sum += (x[i] - (*voidToVector)[i]) * (x[i] - (*voidToVector)[i]);
-            
         }
         std::cout << sum << std::endl;
         return sum;
     }
 
-    //
-    double quaternionConstraint(const std::vector<double> &x, std::vector<double> &grad, void *data)
+    void quaternionConstraint(unsigned m, double *result, unsigned n, const double *x, double *grad, void *data)
     {
-        return sqrt(x[3] * x[3] + x[4] * x[4] + x[5] * x[5] + x[6] * x[6]) - 1;
-    }
 
-    
+        for (unsigned int iq = 0; iq < (unsigned int)m / 3; iq++)
+        {
+            result[iq] = sqrt(x[iq * 7 + 3] * x[iq * 7 + 3] + x[iq * 7 + 4] * x[iq * 7 + 4] + x[iq * 7 + 5] * x[iq * 7 + 5] + x[iq * 7 + 6] * x[iq * 7 + 6]) - 1;
+            // std::cout<<"constraint: "<<result[iq]<<std::endl;
+        }
+    }
 
 }
 
 int main()
 {
-    std::string csvFile = "../programs/MapHumanHandTrajectoryToRobotEndEffector/test-right-arm-motion1.csv";
+    std::string csvFile = "../programs/MapHumanHandTrajectoryToRobotEndEffector/test-right-arm-motion-smooth1.csv";
     std::vector<std::array<double, 8>> desiredTrajectoryData = sharon::getTrajectoryFromCsvFile(csvFile);
     sharon::printTrajectoryData(desiredTrajectoryData);
 
-    std::vector<double> desiredPoses = sharon::getTrajectoryPoses(desiredTrajectoryData, 100);
-    void *parameters = &desiredPoses;
+    unsigned int numPoses = 40;
+    unsigned int sizeDesiredDiscretePoses = numPoses * 7;
+    double desiredDiscretePoses[sizeDesiredDiscretePoses];
 
-    std::vector<double> x(desiredPoses);
-    // x[0] +=0.01;
-    // x[1] += 0.05;
-    std::vector<double> grad(desiredPoses);
+    sharon::getTrajectoryPoses(desiredTrajectoryData, numPoses, desiredDiscretePoses, sizeDesiredDiscretePoses);
 
-    unsigned int nOfVariables = desiredPoses.size();
-    nlopt::opt opt(nlopt::LN_COBYLA, nOfVariables);
-    opt.set_min_objective(sharon::optimizationFunction, parameters);
-    std::vector<double> lb(nOfVariables);
-    std::vector<double> ub(nOfVariables);
 
-    for (unsigned int i = 0; i < nOfVariables; i++)
+
+
+    unsigned int n = sizeDesiredDiscretePoses;
+    double x[n];
+    std::vector<double> vectorX;
+    for (unsigned int i = 0; i < sizeDesiredDiscretePoses; i++)
     {
-        // Our vector x is constructed like [x0 y0 z0 qx0 qy0 qz0 qw0 ... xi yi zi qxi qyi qzi qwi ... xn yn zn qxn qyn qzn qwn]. Where each group of 7 elements
-        // represents a pose
-        if(i<(( (unsigned int) (i/7)+1)*7-4)) // Bounds for position elements [x y z]
-        {   
-            lb[i] = -1.3;
-            ub[i] = 1.3;
-        }
-        else{ // Bounds for quaternion elements [qx qy qz qw]
-            lb[i] = -1;
-            ub[i] = 1.0;
-        }
+        x[i] = desiredDiscretePoses[i];
+        vectorX.push_back(x[i]);
     }
 
-    opt.set_lower_bounds(lb);
-    opt.set_upper_bounds(ub);
+    KDL::Chain chain = makeTeoTrunkAndRightArmKinematicsFromDH();
+    KDL::JntArray qmin, qmax;
+    makeQLimitsTeoTrunkAndRightArmKinematics(qmin, qmax);
+    KDL::ChainFkSolverPos_recursive fksolver(chain);
+    KDL::ChainIkSolverVel_pinv iksolverv(chain);
+    KDL::ChainIkSolverPos_NR_JL iksolver(chain, qmin, qmax, fksolver, iksolverv, 1000);
 
-    opt.add_equality_constraint(sharon::quaternionConstraint, NULL, 1e-6);
+    KDL::JntArray q(chain.getNrOfJoints());
+    KDL::JntArray qInit(chain.getNrOfJoints());
 
-    opt.set_ftol_abs(1e-3);
-    opt.set_xtol_rel(1e-3);
-    opt.set_stopval(1e-5);
 
-    double minf;
-    try
-    {
-        nlopt::result result = opt.optimize(x, minf);
-        std::cout << "found minimum at f(x)=" << minf << std::endl;
-    }
-    catch (std::exception &e)
-    {
-        std::cout << "nlopt failed: " << e.what() << std::endl;
-    }
+    KDL::Frame fGoal;
+    double norm = sqrt(x[3]*x[3] + x[4]*x[4] + x[5]*x[5] + x[6]*x[6]);
+    KDL::Rotation rotKdl = KDL::Rotation::Quaternion(x[3]/norm, x[4]/norm, x[5]/norm, x[6]/norm);
+    KDL::Vector posKdl = KDL::Vector(x[0], x[1], x[2]-0.894);
+    rotKdl = rotKdl*KDL::Rotation::RotX(-KDL::PI/2.0);
+    rotKdl = rotKdl*KDL::Rotation::RotZ(-KDL::PI/2.0-KDL::PI/4.0);
+    fGoal.M = rotKdl;
+    fGoal.p = posKdl;
 
-    // nlopt::opt opt(nlopt::LD_MMA,7); // TODO: Check the optimization method to be used
+    std::cout<<x[0]<<" "<<x[1]<<" "<<x[2]<<std::endl;
+    
 
-    // opt.add_equality_constraint(sharon::quaternionConstraint, NULL, 1e-8);
+
+    int ret = iksolver.CartToJnt(qInit, fGoal, q);
+
+    std::cout<<ret<<std::endl;
+
+    // void *parameters = &vectorX;
+
+    // nlopt_opt opt;
+    // opt = nlopt_create(NLOPT_LN_COBYLA, n);
+    // nlopt_set_min_objective(opt, sharon::optimizationFunction, parameters);
+    // double lb[n];
+    // double ub[n];
+
+    // for (unsigned int i = 0; i < n; i++)
+    // {
+    //     // Our vector x is constructed like [x0 y0 z0 qx0 qy0 qz0 qw0 ... xi yi zi qxi qyi qzi qwi ... xn yn zn qxn qyn qzn qwn]. Where each group of 7 elements
+    //     // represents a pose
+    //     if (i < (((unsigned int)(i / 7) + 1) * 7 - 4)) // Bounds for position elements [x y z]
+    //     {
+    //         lb[i] = -1.3;
+    //         ub[i] = 1.3;
+    //     }
+    //     else
+    //     { // Bounds for quaternion elements [qx qy qz qw]
+    //         lb[i] = -1;
+    //         ub[i] = 1.0;
+    //     }
+    // }
+
+    // nlopt_set_lower_bounds(opt, lb);
+    // nlopt_set_upper_bounds(opt, ub);
+
+    // // //     std::vector<double> tolQuaternionConstraint;
+    // // //     tolQuaternionConstraint.push_back(1e-6);
+
+    // unsigned int m = numPoses * 3;
+    // double tolQuaternionConstraint[m];
+    // for (unsigned int i = 0; i < m; i++)
+    //     tolQuaternionConstraint[i] = 1e-4;
+
+    // nlopt_add_equality_mconstraint(opt, m, sharon::quaternionConstraint, NULL, tolQuaternionConstraint);
+    // nlopt_set_xtol_rel(opt, 1e-5);
+    // nlopt_set_maxtime(opt, 200);
+    // //nlopt_set_xtol_abs(opt, tolQuaternionConstraint);
+
+    // double minf;
+    // try
+    // {
+    //     nlopt_result result = nlopt_optimize(opt, x, &minf);
+    //     std::cout << "found minimum at f(x)=" << minf << std::endl;
+    // }
+    // catch (std::exception &e)
+    // {
+    //     std::cout << "nlopt failed: " << e.what() << std::endl;
+    // }
 
     return 0;
 }
--- programs/MapHumanHandTrajectoryToRobotEndEffector/MapHumanHandTrajectoryToRobotEndEffector.hpp
+++ programs/MapHumanHandTrajectoryToRobotEndEffector/MapHumanHandTrajectoryToRobotEndEffector.hpp
@@ -20,16 +20,18 @@ namespace sharon{
      * @param data Trajectory data stored as blocks of 8 elements [frame, x, y, z, qx, qy, qz, qw]
      */
     void printTrajectoryData(const std::vector<std::array<double,8>> &data);
-    
+
     /**
      * @brief Get the Trajectory Poses object
      * 
      * @param trajectoryData  Trajectory data stored as blocks of 8 elements [frame, x, y, z, qx, qy, qz, qw]
      * @param numPoses Number of poses of the initial trajectory to generate the discrete trajectory.
-     * @return  Discrete trajectory constructed as a vector with numPoses*7 elements. Each group of 7 elements in the vector represent one discrete pose. 
+     * @param discreteTrajectory Discrete trajectory constructed as a array with numPoses*7 elements. Each group of 7 elements in the vector represent one discrete pose. 
+     * @param size size of the discrete trajectory arrray
      */
-    std::vector<double> getTrajectoryPoses(const std::vector<std::array<double,8>>& trajectoryData, const unsigned int &numPoses);
+    void getTrajectoryPoses(const std::vector<std::array<double, 8>> &trajectoryData, const unsigned int &numPoses, double *discreteTrajectory, unsigned int &size);
     
+
     /**
      * @brief Optimization function to minimize the distance between the pose of the desired trajectory and the final trajectory that satisfies the constraints.
      * This function is defined to be used with nlopt library. 
@@ -39,17 +41,31 @@ namespace sharon{
      * @param data Additional data to be used in the optimization function.
      * @return Value of the optimization function at x.
      */
-    double optimizationFunction(const std::vector<double> &x, std::vector<double>&grad, void * data);
 
     /**
-     * @brief Constraint for only allowing normalized quaternions.
+     * @brief Optimization function to minimize the distance between the pose of the desired trajectory and the final trajectory that satisfies the constraints.
+     * This function is defined to be used with nlopt library. 
      * 
-     * @param x Optimation parameters that represent the final trajectory. This trajectory is a row vector, where each group of 7 continuous elements represent one pose.   
-     * @param grad Gradient of the optimization function with respect to the optimization parameters.
-     * @param data Additional data to be used in the constraint function.
-     * @return norm(quaternion)-1
+     * @param n number of parameters
+     * @param x Optimization parameters.
+     * @param grad Gradient of the optimization function with respect to the optimization pararameters.
+     * @param data Additional data to be used in the optimization function.
+     * @return Value of the optimization function at x. 
      */
-    double quaternionConstraint(const std::vector<double> &x, std::vector<double> &grad, void *data);
+
+    double optimizationFunction(unsigned n, const double *x, double * grad, void * data);
+
+    /**
+     * @brief 
+     * 
+     * @param m 
+     * @param result 
+     * @param n 
+     * @param x 
+     * @param grad 
+     * @param data 
+     */
+    void quaternionConstraint(unsigned m,double * result, unsigned n, const double *x, double * grad, void *data);
 
 }// namespace sharon
 
